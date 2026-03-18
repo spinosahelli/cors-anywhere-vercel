a0227674c0b2d72550b5de72d1e43906d505e82f
spinosahelli <spinosahelli@gmail.com>
2026-03-18 09:15:09 +0800

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,6 +1,6 @@
 const corsProxy = require('cors-anywhere');
 const proxy = corsProxy.createServer({
-    originWhitelist: ['https://dav.jianguoyun.com/dav/', 'https://habit.loc.cc/'],
+    originWhitelist: ['https://dav.jianguoyun.com/dav/', 'https://daka.995266.xyz/'],
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 });
